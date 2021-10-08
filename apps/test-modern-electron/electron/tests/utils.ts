0962c187817da91b5bccde5763482be504e0130b
皮锟 <pikun@bytedance.com>
2021-10-08 10:44:01 +0800

    fix: add webpack as dev dependences in test-modern-electron to fix running problem

--- apps/test-modern-electron/electron/tests/utils.ts
+++ apps/test-modern-electron/electron/tests/utils.ts
@@ -1,36 +1,33 @@
 import { join } from 'path';
 
-export const sleep = (sec: number) => {
-  return new Promise(resolve => {
+export const sleep = (sec: number) =>
+  new Promise(resolve => {
     setTimeout(() => resolve(true), sec * 1000);
   });
-};
 
 export const getOpenWindowConfig = (
   winName: string,
   customConfig: Record<string, any> = {},
-) => {
-  return {
-    funcName: 'winService.createWindow',
-    args: [
-      {
-        name: winName,
-        options: {
-          width: 400,
-          webPreferences: {
-            enableRemoteModule: true,
-            preload: join(
-              __dirname,
-              '..',
-              'preload',
-              'browserWindow',
-              'index.dev.js',
-            ),
-          },
+) => ({
+  funcName: 'winService.createWindow',
+  args: [
+    {
+      name: winName,
+      options: {
+        width: 400,
+        webPreferences: {
+          enableRemoteModule: true,
+          preload: join(
+            __dirname,
+            '..',
+            'preload',
+            'browserWindow',
+            'index.dev.js',
+          ),
         },
-        loadUrl: 'https://www.baidu.com',
-        ...customConfig,
       },
-    ],
-  };
-};
+      loadUrl: 'https://www.baidu.com',
+      ...customConfig,
+    },
+  ],
+});
