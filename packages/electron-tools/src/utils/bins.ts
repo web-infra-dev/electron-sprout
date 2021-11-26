061df42209043be56d57b37b73a7294e90cf4003
pikun <1457431899@qq.com>
2021-11-26 09:45:51 +0800

    Fix build main cmd (#56)
    * fix: fix build main with extra folder
    
    * fix: package.json dist path
    
    Co-authored-by: 皮锟 <pikun@bytedance.com>

--- packages/electron-tools/src/utils/bins.ts
+++ packages/electron-tools/src/utils/bins.ts
@@ -1,3 +1,17 @@
 import os from 'os';
 
 export const YARN_BIN = os.platform() === 'darwin' ? 'yarn' : 'yarn.cmd';
+
+export const getElectronBuilderBin = () => {
+  try {
+    // electron-builder >= 22.13
+    return require.resolve('electron-builder/cli.js');
+  } catch (error) {
+    // electron-builder >= 22.10
+    try {
+      return require.resolve('electron-builder/out/cli/cli.js');
+    } catch (_err) {
+      throw Error('electron-builder version should >=22.10.0');
+    }
+  }
+};
