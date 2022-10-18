3c7536ee24b85cdee8c7f08c750f32d7666d6cfd
zjk <jingkai.zhao@foxmail.com>
2022-10-18 11:42:31 +0800

    feat: allow to change runtime and plugin package name

--- packages/electron-tools/src/utils/constant.ts
+++ packages/electron-tools/src/utils/constant.ts
@@ -5,4 +5,6 @@ export const ENV_NAME = {
 
 export const ENVS = {
   ELECTRON_BUILD_ENV: 'ELECTRON_BUILD_ENV', // build with env: [development、production]，it's not same as NODE_ENV. it means -d option
+  ELECTRON_RUNTIME_PKG_NAME: 'ELECTRON_RUNTIME_PKG_NAME',
+  ELECTRON_PLUGIN_PKG_NAME: 'ELECTRON_PLUGIN_PKG_NAME',
 };
